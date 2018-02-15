79b20fb7b14d51458250c2a940a523282139df82
Fabian Ardila Rodriguez <fabianra97@gmail.com>
2018-02-15 03:46:40 -0500

    Creación de pruebas

--- src/test/java/edu/eci/arsw/myrestaurant/test/ApplicationServicesTests.java
+++ src/test/java/edu/eci/arsw/myrestaurant/test/ApplicationServicesTests.java
@@ -3,6 +3,7 @@ package edu.eci.arsw.myrestaurant.test;
 import edu.eci.arsw.myrestaurant.beans.BillCalculator;
 import edu.eci.arsw.myrestaurant.model.Order;
 import edu.eci.arsw.myrestaurant.services.OrderServicesException;
+import edu.eci.arsw.myrestaurant.services.RestaurantOrderServices;
 import edu.eci.arsw.myrestaurant.services.RestaurantOrderServicesStub;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,21 +13,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest()
 public class ApplicationServicesTests {
 
-    
-    RestaurantOrderServicesStub ros;
+    @Autowired
+    RestaurantOrderServices ros;
 
     
     @Test
     public void contextLoads() throws OrderServicesException{
+        Order o = new Order(2);
+        o.addDish("PIZZA", 3);
+        o.addDish("HOTDOG", 1);
+        o.addDish("COKE", 4);
+        o.addDish("BEER", 5);
+        ros.addNewOrderToTable(o);
         
-        
-        
-        
+        assertEquals("Impuesto calculado incorrectameente.", 59802,ros.calculateTableBill(2));
     }
 
 }
